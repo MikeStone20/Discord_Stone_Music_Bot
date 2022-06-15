a06719668ca9ff6cddc971fb53195918c9c265aa
MikeStone20 <michael.stone0320@gmail.com>
2022-06-15 00:01:20 -0600

    basic response bot

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('hello world')
+import discord
+import youtube_dl
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    pass
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hello")
+
+client.run('OTg2NDA4MDg5Mzk1NzkzOTcy.GdQ04H.3FC9UodcWV71RkVSMDNFehCz5Wcsq_0Q35lHfE')
